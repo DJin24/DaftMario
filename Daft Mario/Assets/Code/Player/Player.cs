d367c782490d1439c544c9c9b25c4a03ac9bddfc
DJin24 <davidchristopherjin@gmail.com>
2020-12-02 22:25:23 -0600

    Removed all references to SimplePhysics

--- Daft Mario/Assets/Code/Player/Player.cs
+++ Daft Mario/Assets/Code/Player/Player.cs
@@ -7,41 +7,46 @@ namespace Assets.Code.Player
     {
         private static readonly Vector2 Acceleration = new Vector2(2f, 0f);
         private static readonly Vector2 JumpVelocity = new Vector2(0f, 10f);
-
-        private SimplePhysics _physics;
+        private Rigidbody2D _rb;
         private bool _jumping; // are we jumping?
 
         internal void Start () {
-            _physics = GetComponent<SimplePhysics>();
-            _physics.CollisionDown += CollisionDown;
-
+            _rb = GetComponent<Rigidbody2D>();
         }
 
         internal void Update () {
             CheckKeys();
-            if (!_jumping) { _physics.AddVelocity(Acceleration * Time.deltaTime); } // accelerate when we're on the ground
         }
 
         private void CheckKeys () {
             if (Input.GetKeyDown(KeyCode.P)) { Game.Ctx.Clock.TogglePause(); }
             if (Input.GetKeyDown(KeyCode.Space)) { Jump(); }
+            if (Input.GetKeyDown(KeyCode.A)) { Left(); }
+            if (Input.GetKeyDown(KeyCode.D)) { Right(); }
+            else {
+                _rb.velocity = new Vector2(0f, _rb.velocity.y);
+            }
         }
 
         private void CollisionDown (Collider2D other) {
-            var platform = other.gameObject.GetComponent<Platform>();
-            if (platform == null) { return; } // shouldn't happen ;)
-
-            if (!platform.LandedOn) {
-                platform.Land();
-                // score stuff
-            }
-
             _jumping = false;
         }
 
         private void Jump () {
-            if (!_jumping) { _physics.AddVelocity(JumpVelocity); }
+            if (!_jumping) {
+                _rb.velocity += JumpVelocity;
+            }
             _jumping = true;
+        }
+
+        private void Left() {
+            // Set sprite to face left
+            _rb.velocity = new Vector2(-1f, _rb.velocity.y);
+        }
+
+        private void Right() {
+            // Set sprite to face right
+            _rb.velocity = new Vector2(1f, _rb.velocity.y);
         }
     }
 }
