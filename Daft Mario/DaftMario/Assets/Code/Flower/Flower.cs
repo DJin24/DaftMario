3176837501bbc542222319d3a14fda48f7651b6b
DJin24 <davidchristopherjin@gmail.com>
2020-12-04 15:06:11 -0600

    Fixed aspect ratio

---
+++ Daft Mario/DaftMario/Assets/Code/Flower/Flower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour
+{
+    public float amplitude = 1.0f;
+    public float frequency = 1.0f;
+    private float delay;
+    private Vector3 startPos;
+    void Start()
+    {
+        delay = Random.Range(0f, 20f);
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 pos = startPos;
+        pos.y += amplitude * Mathf.Sin ((Time.time + delay) * frequency);
+        transform.position = pos;
+    }
+}
---
+++ Daft Mario/DaftMario/Assets/Code/ScoreKeeper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private static Text display;
+    private static int score = 0;
+    internal void Start ()
+    {
+        display = GetComponent<Text>();
+        Refresh();
+    }
+
+    public static void AddToScore()
+    {
+        score += 1;
+        if (score >= 5) { Win(); }
+        else { Refresh(); }
+    }
+
+    public static void ResetScore()
+    {
+        score = 0;
+        Refresh();
+    }
+
+    public static void Win()
+    {
+        display.text = "You win! Press ESC to reset.";
+        Time.timeScale = 0f;
+    }
+    
+    public static void Lose()
+    {
+        display.text = "You died. Press ESC to reset.";
+        Time.timeScale = 0f;
+    }
+    
+    private static void Refresh()
+    {
+        display.text = $"Score: {score}";
+    }
+}
